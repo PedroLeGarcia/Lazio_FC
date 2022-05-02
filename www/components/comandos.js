573a296f87ecda5f80e22e5c7ac349c78cd6ae42
PedroLeGarcia <bstoolk@gmail.com>
2022-05-03 00:20:56 +0900

    CODIGO COMPLETO

---
+++ www/components/comandos.js
@@ -0,0 +1,31 @@
+// This is Kane West
+window.onload = function(){
+    document.querySelector("#scan").addEventListener('click', function(){
+   cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          if(result.text == '1234'){
+              document.querySelector("#dados").innerHTML = "<h1> ACHOU O PRODUTO!!!!! </h1>";
+          }else{
+              document.querySelector("#dados").innerHTML = "<h1> INSIRA O PRODUTO CERTO! </h1>";
+          }
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+     );
+    });
+}
+
