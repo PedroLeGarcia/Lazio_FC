e7868328eece36e18acc55bdbb087f5d322eeb7b
PedroLeGarcia <bstoolk@gmail.com>
2022-05-10 08:05:20 +0900

    eu to realmente louco

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -1,6 +1,6 @@
 // This is Kane West
 window.onload = function(){
-    document.querySelector("#scan").addEventListener('click', function(){
+    document.querySelector("#button1").addEventListener('click', function(){
    cordova.plugins.barcodeScanner.scan(
       function (result) {
           if(result.text == '280720550'){
@@ -12,9 +12,9 @@ window.onload = function(){
               document.querySelector("#dados") .innerHTML = "<img src=/components/imagens/Imagem3.jpg> <div class='container'> <p> Motorola One XT1941 Branco 64GB </p> <p> Valor: R$ 1200,00 </p> <p> Sistema Operacional Android 8.1 Oreo </p> <p> Processador Qualcomm Snapdragon 625 (MSM8953) Octa-Core de 2.0 GHz </p> <p> RAM 4GB </p> <p> Tecnologia GSM, 3G e 4G </p> <p> Memória interna 64 GB Total sendo uma parte usada para o sistema operacional e aplicativos pré-instalados </p> <p> Cor Branco </p> <p> Características Gerais-Tela Max Vision e design ao máximo-Câmera dupla e armazenamento ilimitado-Android One: primeiro do Brasil, atualizado e seguro-Carregamento ultrarrápido TurboPower-Últimas inovações do Google incorporadas-Leitor Biométrico (Impressão Digital) </p> </div>";
 
           }else if(result.text == '85369877444'){
-              document.querySelector("#dados") .innerHTML = "<img src=components/imagens/Imagem4.jpg> <div class='container'> <p> Asus Zenfone Max Pro M1 </p> <p> Valor: R$ 1.199,00 </p> <p> Processador: Qualcomm Snapdragon SDM636 1,8 GHz, 2 Mb Cache </p> <p> Sistema Operacional: Android 8.1 Oreo </p> <p> Memória Ram: 4 Gb </p> <p> Memória Interna: 64 Gb  </p> <p> Memória Expansiva: 2TB Sd </p> <p> Tela: 6,0 Led Backlight Ips panel (1080x2160) </p> <p> Tipo de Chip: Nano -Dual Chip: Sim </p> <p>Conectividade: 3G; 4G; Wi-fi</p> <p>Câmera Frontal: 8MP</p> <p>Câmera Traseira: 16+5MP</p> </div>"; 
+              document.querySelector("#dados").innerHTML = "<img src=components/imagens/Imagem4.jpg> <div class='container'> <p> Asus Zenfone Max Pro M1 </p> <p> Valor: R$ 1.199,00 </p> <p> Processador: Qualcomm Snapdragon SDM636 1,8 GHz, 2 Mb Cache </p> <p> Sistema Operacional: Android 8.1 Oreo </p> <p> Memória Ram: 4 Gb </p> <p> Memória Interna: 64 Gb  </p> <p> Memória Expansiva: 2TB Sd </p> <p> Tela: 6,0 Led Backlight Ips panel (1080x2160) </p> <p> Tipo de Chip: Nano -Dual Chip: Sim </p> <p>Conectividade: 3G; 4G; Wi-fi</p> <p>Câmera Frontal: 8MP</p> <p>Câmera Traseira: 16+5MP</p> </div>"; 
           }else{
-               document.querySelector("#dados").innerHTML = "<h1>Produto não encontrado</h1>";
+               document.querySelector("#dados").innerHTML = "<h1>PRODUTO NÃO ENCONTRADO!</h1>";
           }
       },
       function (error) {
